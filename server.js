1cced61df1fa32ac68a4524718242639a3b5ef42
bento-ux <bentodotco@gmail.com>
2025-02-17 14:22:06 +0700

    update tambahin kata bens
    Signed-off-by: bento-ux <bentodotco@gmail.com>

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const {greeting} = require('./user')
 
 app.get('/', (req,res) => res.send('This App is running properly!'))
-app.get('/ping', (req,res) => res.send('Pong Ben!'))
+app.get('/ping', (req,res) => res.send('Pong Bens!'))
 app.get('/twitter', (req,res) => res.send('Hallo twitter!'))
 app.get('/hello/:name', (req,res) => {
     res.json({message:greeting(req.params.name)})
